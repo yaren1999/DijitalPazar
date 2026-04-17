b2b9e14d6013eba32e68cc272f6db0222800cbd7
yaren1999 <demirciyarenmisra@gmail.com>
2026-04-17 14:41:07 +0300

    SecureToken güncellendi ve Pausable eklendi

--- contracts/SecureToken.sol
+++ contracts/SecureToken.sol
@@ -4,8 +4,9 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
 
-contract SecureToken is ERC20, Ownable, ERC20Burnable {
+contract SecureToken is ERC20, Ownable, ERC20Burnable, Pausable {
     
     constructor(uint256 initialSupply) 
         ERC20("Dijital Pazar Token", "DPT") 
@@ -14,7 +15,19 @@ contract SecureToken is ERC20, Ownable, ERC20Burnable {
         _mint(msg.sender, initialSupply); 
     }
 
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
+    }
+
+    function _update(address from, address to, uint256 value) internal virtual override whenNotPaused {
+        super._update(from, to, value);
     }
 }
